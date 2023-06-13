5c42fb1e4e32722aa6b4e9f7caa99d2b08f1262d
facuaragon <facundo_aragon@hotmail.com>
2023-06-13 11:20:53 -0300

    change colors and footer

--- app.js
+++ app.js
@@ -123,12 +123,6 @@ app.post("/delete", async function(req, res){
   }
 })
 
-
-
-// app.get("/about", function(req, res){
-//   res.render("about");
-// });
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
